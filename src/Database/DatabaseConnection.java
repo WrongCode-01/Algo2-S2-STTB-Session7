a4f5e8366c1cde43ef4d3c1d36fb5ab541877815
Muhammad Dena <91838525+WrongCode-01@users.noreply.github.com>
2022-04-21 04:18:42 +0700

    Add files via upload

---
+++ src/Database/DatabaseConnection.java
@@ -0,0 +1,36 @@
+package Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MDena
+ * https://github.com/WrongCode-01?tab=repositories
+ */
+public class DatabaseConnection {
+   private static Connection mysqlconfig;
+    public static Connection configDB()throws SQLException{
+        try {
+            String url="jdbc:mysql://localhost:3306/db_sekolah"; //url database
+            String user="root"; //user database
+            String pass=""; //password database
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            mysqlconfig=DriverManager.getConnection(url, user, pass);            
+        } catch (Exception e) {
+            System.err.println("koneksi gagal "+e.getMessage()); //perintah menampilkan error pada koneksi
+        }
+        return mysqlconfig;
+    }    
+}
